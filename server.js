c8e7eca923f6f17df6e6f1fff6d9da696de76bdd
JoshBirnbaum <jtbirnbaum@bsu.edu>
2020-06-15 15:54:21 -0400

    ipdate database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-vertical-89097',
 		user: 'postgres',
 		password: 'jtthebest36',
 		database: 'smartbrain'
